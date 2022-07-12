4987124bd9d2077826c7acd55ad2e58c62057b2f
lucianatabassi <81652066+lucianatabassi@users.noreply.github.com>
2022-07-12 03:49:01 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$('section.awSlider .carousel').carousel({
+/*$('section.awSlider .carousel').carousel({
 	pause: "hover",
   interval: 2000
 });
@@ -9,4 +9,35 @@ $('section.awSlider').append('<img src="' + startImage + '">');
 $('section.awSlider .carousel').on('slid.bs.carousel', function () {
  var bscn = $(this).find('.item.active > img').attr('src');
 	$('section.awSlider > img').attr('src',bscn);
-});
+});*/
+
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
